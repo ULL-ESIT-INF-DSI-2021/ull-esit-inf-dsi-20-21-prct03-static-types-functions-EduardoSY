f5ff3cf6931c0af9c27be262bd3a6516a118ac6f
Eduardo Da Silva <alu0101104911@ull.edu.es>
2021-03-07 02:09:42 +0000

    Ejercicio 5 hecho

---
+++ dist/ejercicio5.js
@@ -0,0 +1,11 @@
+// Ejercicio 5: Chuck Norris quiere en una linea de codigo:
+// 1. Eliminar "a" y "e" de las palabras
+// 2. Separar, ordenar esas palabras por orden alfabético y reunirlas
+// 3. Que la cadena vacia muestre "Broken!"
+function onePunch(cadena) {
+    return ((cadena.length === 0) ?
+        'Broken!' : cadena.replace(/[ae]/g, '').split(' ').sort().join(' '));
+}
+console.log(onePunch('Beard Jeans Hairbrush Knuckleduster Sand'));
+console.log(onePunch('Snot Snow Soda Tank Beard'));
+console.log(onePunch(''));
