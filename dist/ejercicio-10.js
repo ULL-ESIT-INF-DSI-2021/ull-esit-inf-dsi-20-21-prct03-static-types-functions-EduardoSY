ed1d3e439685281ac1da6d623dad077a85381b1e
Eduardo Da Silva <alu0101104911@ull.edu.es>
2021-03-07 06:51:06 +0000

    Ejercicio 10 en proceso

---
+++ dist/ejercicio-10.js
@@ -0,0 +1,16 @@
+// Ejercicio 10: Validador de nombres de usuario
+// Me rindo intentando sacar una expresion que haga todo eso
+function isValidUsername(nombre) {
+    let size = nombre.length;
+    if ((size >= 4) && (size <= 30)) {
+        if (/^(?!_).*(?<!_)$/g.test(nombre)) {
+            if (/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$_-])/g.test(nombre)) {
+                if (/(?![a-zA-Z]{3,})(?![0-9]{3,})(?![$_-]{3,})/g.test(nombre)) {
+                }
+            }
+            return true;
+        }
+    }
+    return false;
+}
+console.log(isValidUsername('jola_'));
